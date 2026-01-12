da81538ea2a002f17c81684f5c3812ad6d255fa5
projectholsey <matty-sep@hotmail.com>
2026-01-11 22:24:25 -0500

    Attempting to use the inotify library - but you should learn debugging prior to this so we actually know what this is doing..

--- main.c
+++ main.c
@@ -3,7 +3,13 @@
 #include <string.h>
 #include <stdlib.h>
 
+#include <stdarg.h>
 
+#include <sys/inotify.h> // system library to watch the file for changes
+
+
+
+#define BUF_LEN (10 * (sizeof(struct inotify_event) + NAME_MAX + 1))
 
 const char *STANDARD_LOGFILE_NAME = "projectMonitorOut.txt";
 
@@ -19,8 +25,37 @@ struct trackerEnum {
 typedef struct trackerEnum FTracker;
 
 
+
 // prototype declarations
 int checkValidFileIn(int argc, char *argv[], FTracker *newTracker);
+
+void displayInotifyEvent(struct inotify_event *i) {
+    printf("    wd =%2d; ", i->wd);
+    if (i->cookie > 0)
+        printf("cookie =%4d; ", i->cookie);
+
+    printf("mask = ");
+    if (i->mask & IN_ACCESS)        printf("IN_ACCESS ");
+    if (i->mask & IN_ATTRIB)        printf("IN_ATTRIB ");
+    if (i->mask & IN_CLOSE_NOWRITE) printf("IN_CLOSE_NOWRITE ");
+    if (i->mask & IN_CLOSE_WRITE)   printf("IN_CLOSE_WRITE ");
+    if (i->mask & IN_CREATE)        printf("IN_CREATE ");
+    if (i->mask & IN_DELETE)        printf("IN_DELETE ");
+    if (i->mask & IN_DELETE_SELF)   printf("IN_DELETE_SELF ");
+    if (i->mask & IN_IGNORED)       printf("IN_IGNORED ");
+    if (i->mask & IN_ISDIR)         printf("IN_ISDIR ");
+    if (i->mask & IN_MODIFY)        printf("IN_MODIFY ");
+    if (i->mask & IN_MOVE_SELF)     printf("IN_MOVE_SELF ");
+    if (i->mask & IN_MOVED_FROM)    printf("IN_MOVED_FROM ");
+    if (i->mask & IN_MOVED_TO)      printf("IN_MOVED_TO ");
+    if (i->mask & IN_OPEN)          printf("IN_OPEN ");
+    if (i->mask & IN_Q_OVERFLOW)    printf("IN_Q_OVERFLOW ");
+    if (i->mask & IN_UNMOUNT)       printf("IN_UNMOUNT ");
+    printf("\n");
+
+    if (i->len > 0)
+        printf("        name = %s\n", i->name);
+}
 
 
 int main(int argc, char *argv[]) {
@@ -39,6 +74,7 @@ int main(int argc, char *argv[]) {
     
     FTracker newTracker;
     FTracker *ntptr = &newTracker;
+    struct inotify_event *event;
 
 
     printf("Starting file\n");
@@ -57,8 +93,11 @@ int main(int argc, char *argv[]) {
 
 int checkValidFileIn(int argc, char *argv[], FTracker *newTracker) {
 
+    int inotifyFd, wd, j;
+    size_t numRead;
     FILE *newFile;
     int found = 0;
+    
 
     for (int i = 0 ; i < argc; i++) {
         if (strcmp("-f", argv[i]) == 0) {
@@ -90,10 +129,40 @@ int checkValidFileIn(int argc, char *argv[], FTracker *newTracker) {
     }
 
 
+    /**
+     * NEWLY ADDED STUFF IS HERE
+     * 
+     */
+
+    inotifyFd = inotify_init();                 // Create inotify instance 
+    if (inotifyFd == -1){
+        printf("inotify init");
+        exit(EXIT_FAILURE);
+    }        
+
+    
+    wd = inotify_add_watch(inotifyFd, newTracker->file_to_track, IN_ALL_EVENTS);
+    if (wd == -1){
+        printf("inotify_add_watch");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Watching %s using wd %d\n", argv[j], wd);
+    
+
+    
+
+    // while(1){
+        
+    // }
+
+
     // closing the file on exit
     fclose(newFile);
     
+    exit(EXIT_SUCCESS);
 
     return 0; // VALID
 
 }
+
