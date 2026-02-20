a0b240ea516a827cc60e9f0f69b931339b364f70
projectHolsey <matty-sep@hotmail.com>
2026-02-19 20:43:48 -0500

    Added new functionality for inotify library - monitors file passed, waits for enter key input to close loop, reports all access/open/close/modify events on file

--- main.c
+++ main.c
@@ -3,6 +3,10 @@
 #include <string.h>
 #include <stdlib.h>
 
+#include <errno.h>
+#include <unistd.h>
+#include <poll.h>
+
 #include <stdarg.h>
 
 #include <sys/inotify.h> // system library to watch the file for changes
@@ -29,32 +33,85 @@ typedef struct trackerEnum FTracker;
 // prototype declarations
 int checkValidFileIn(int argc, char *argv[], FTracker *newTracker);
 
-void displayInotifyEvent(struct inotify_event *i) {
-    printf("    wd =%2d; ", i->wd);
-    if (i->cookie > 0)
-        printf("cookie =%4d; ", i->cookie);
+/* Read all available inotify events from the file descriptor 'fd'.
+    wd is the table of watch descriptors for the directories in argv.
+    argc is the size of wd and argv.
+    argv is the list of watched directories.
+    Entry 0 of wd and argv is unused.  */
 
-    printf("mask = ");
-    if (i->mask & IN_ACCESS)        printf("IN_ACCESS ");
-    if (i->mask & IN_ATTRIB)        printf("IN_ATTRIB ");
-    if (i->mask & IN_CLOSE_NOWRITE) printf("IN_CLOSE_NOWRITE ");
-    if (i->mask & IN_CLOSE_WRITE)   printf("IN_CLOSE_WRITE ");
-    if (i->mask & IN_CREATE)        printf("IN_CREATE ");
-    if (i->mask & IN_DELETE)        printf("IN_DELETE ");
-    if (i->mask & IN_DELETE_SELF)   printf("IN_DELETE_SELF ");
-    if (i->mask & IN_IGNORED)       printf("IN_IGNORED ");
-    if (i->mask & IN_ISDIR)         printf("IN_ISDIR ");
-    if (i->mask & IN_MODIFY)        printf("IN_MODIFY ");
-    if (i->mask & IN_MOVE_SELF)     printf("IN_MOVE_SELF ");
-    if (i->mask & IN_MOVED_FROM)    printf("IN_MOVED_FROM ");
-    if (i->mask & IN_MOVED_TO)      printf("IN_MOVED_TO ");
-    if (i->mask & IN_OPEN)          printf("IN_OPEN ");
-    if (i->mask & IN_Q_OVERFLOW)    printf("IN_Q_OVERFLOW ");
-    if (i->mask & IN_UNMOUNT)       printf("IN_UNMOUNT ");
-    printf("\n");
+static void handle_events(int fd, int *wd, int argc, char* argv[]) {
+    /* Some systems cannot read integer variables if they are not
+        properly aligned.  On other systems, incorrect alignment may
+        decrease performance.  Hence, the buffer used for reading from
+        the inotify file descriptor should have the same alignment as
+        struct inotify_event.  */
 
-    if (i->len > 0)
-        printf("        name = %s\n", i->name);
+    char buf[4096]
+        __attribute__ ((aligned(__alignof__(struct inotify_event))));
+    const struct inotify_event *event;
+    ssize_t size;
+
+    /* Loop while events can be read from inotify file descriptor.  */
+
+    for (;;) {
+
+        /* Read some events.  */
+
+        size = read(fd, buf, sizeof(buf));
+        if (size == -1 && errno != EAGAIN) {
+            perror("read");
+            exit(EXIT_FAILURE);
+        }
+
+        /* If the nonblocking read() found no events to read, then
+            it returns -1 with errno set to EAGAIN.  In that case,
+            we exit the loop.  */
+
+        if (size <= 0)
+            break;
+
+        /* Loop over all events in the buffer.  */
+
+        for (char *ptr = buf; ptr < buf + size;
+                ptr += sizeof(struct inotify_event) + event->len) {
+
+            event = (const struct inotify_event *) ptr;
+
+            /* Print event type.  */
+
+            if (event->mask & IN_OPEN)
+                printf("IN_OPEN: ");
+            if (event->mask & IN_CLOSE_NOWRITE)
+                printf("IN_CLOSE_NOWRITE: ");
+            if (event->mask & IN_CLOSE_WRITE)
+                printf("IN_CLOSE_WRITE: ");
+            if (event->mask & IN_MODIFY)
+                printf("IN_MODIFY: ");
+            if (event->mask & IN_ACCESS)
+                printf("IN_ACCESS: ");
+
+            /* Print the name of the watched directory.  */
+
+            for (size_t i = 1; i < argc; ++i) {
+                if (wd[i] == event->wd) {
+                    printf("%s/", argv[i]);
+                    break;
+                }
+            }
+
+            /* Print the name of the file.  */
+
+            if (event->len)
+                printf("%s", event->name);
+
+            /* Print type of filesystem object.  */
+
+            if (event->mask & IN_ISDIR)
+                printf(" [directory]\n");
+            else
+                printf(" [file]\n");
+        }
+    }
 }
 
 
@@ -76,6 +133,28 @@ int main(int argc, char *argv[]) {
     FTracker *ntptr = &newTracker;
     struct inotify_event *event;
 
+    char buf;
+    int fd; // file descriptor 
+    int i;
+    int poll_num;
+    int *wd;
+    nfds_t nfds;
+    struct pollfd fds[2];
+
+    // Creating the file descriptor for accessing inotify api
+    fd = inotify_init1(IN_NONBLOCK);
+    if (fd == -1) {
+        perror("inotify_init1");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Allocate memory for watch descriptors.  */
+    wd = calloc(argc, sizeof(int));
+    if (wd == NULL) {
+        perror("calloc");
+        exit(EXIT_FAILURE);
+    }
+
 
     printf("Starting file\n");
     printf("Found %d arguments\n", argc);
@@ -83,8 +162,59 @@ int main(int argc, char *argv[]) {
     if (checkValidFileIn(argc, argv, ntptr) == 1) {
         return 1;
     }
+
+    // Watch the new file
+    wd[i] = inotify_add_watch(fd, ntptr->file_to_track, IN_OPEN | IN_CLOSE | IN_ACCESS | IN_MODIFY);
     
-    
+
+    // Checking the watcher started correctly
+    if (wd[i] == -1) {
+        fprintf(stderr, "Cannot watch '%s': %s\n", ntptr->file_to_track, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+
+     /* Prepare for polling.  */
+
+    nfds = 2;
+
+    fds[0].fd = STDIN_FILENO;       /* Console input */
+    fds[0].events = POLLIN;
+
+    fds[1].fd = fd;                 /* Inotify input */
+    fds[1].events = POLLIN;
+
+    /* Wait for events and/or terminal input.  */
+
+    printf("Listening for events.\n");
+    while (1) {
+        poll_num = poll(fds, nfds, -1);
+        if (poll_num == -1) {
+            if (errno == EINTR)
+                continue;
+            perror("poll");
+            exit(EXIT_FAILURE);
+        }
+
+        if (poll_num > 0) {
+
+            if (fds[0].revents & POLLIN) {
+
+                /* Console input is available.  Empty stdin and quit.  */
+
+                while (read(STDIN_FILENO, &buf, 1) > 0 && buf != '\n')
+                    continue;
+                break;
+            }
+
+            if (fds[1].revents & POLLIN) {
+
+                /* Inotify events are available.  */
+
+                handle_events(fd, wd, argc, argv);
+            }
+        }
+    }
     
 
     return 0;
@@ -126,41 +256,12 @@ int checkValidFileIn(int argc, char *argv[], FTracker *newTracker) {
         // fclose(newFile);
         return 1;
         
-    }
-
-
-    /**
-     * NEWLY ADDED STUFF IS HERE
-     * 
-     */
-
-    inotifyFd = inotify_init();                 // Create inotify instance 
-    if (inotifyFd == -1){
-        printf("inotify init");
-        exit(EXIT_FAILURE);
-    }        
-
+    }    
     
-    wd = inotify_add_watch(inotifyFd, newTracker->file_to_track, IN_ALL_EVENTS);
-    if (wd == -1){
-        printf("inotify_add_watch");
-        exit(EXIT_FAILURE);
-    }
-
-    printf("Watching %s using wd %d\n", argv[j], wd);
-    
-
-    
-
-    // while(1){
-        
-    // }
-
 
     // closing the file on exit
     fclose(newFile);
     
-    exit(EXIT_SUCCESS);
 
     return 0; // VALID
 
